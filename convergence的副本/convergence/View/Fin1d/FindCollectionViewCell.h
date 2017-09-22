f62893d27e46824674b04212adfe8887139bdc20
admin <3147358955@qq.com>
2017-09-22 18:03:55 +0800

    2

---
+++ convergence的副本/convergence/View/Fin1d/FindCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  FindCollectionViewCell.h
+//  convergence
+//
+//  Created by admin on 2017/9/6.
+//  Copyright © 2017年 adminadmineducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *contentImage;
+@property (weak, nonatomic) IBOutlet UILabel *shopLbl;
+@property (weak, nonatomic) IBOutlet UILabel *addressLbl;
+@property (weak, nonatomic) IBOutlet UILabel *disLbl;
+
+@end
