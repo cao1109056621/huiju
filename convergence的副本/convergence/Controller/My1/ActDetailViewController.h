8b026129f41bd6f005a2f2aeacaf31a06e18e398
yuhaun <1625283223@qq.com>
2017-09-22 18:01:11 +0800

    0000

---
+++ convergence的副本/convergence/Controller/My1/ActDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActDetailViewController.h
+//  convergence
+//
+//  Created by admin on 2017/9/16.
+//  Copyright © 2017年 adminadmineducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ActModel.h"
+@interface ActDetailViewController : UIViewController
+@property(strong,nonatomic) ActModel * activity;
+@end
