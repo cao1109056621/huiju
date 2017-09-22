cce1817e66ad7828dc5d50676ad1ad825cc2451e
yangxiaoqing <932237153@qq.com>
2017-09-22 18:09:21 +0800

    1

---
+++ convergence的副本/convergence/Controller/未命名文件夹/Find/FindViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindViewController.h
+//  convergence
+//
+//  Created by admin on 2017/9/6.
+//  Copyright © 2017年 adminadmineducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HomeModel.h"
+@interface FindViewController : UIViewController
+@property(strong,nonatomic) HomeModel * home;
+@end
