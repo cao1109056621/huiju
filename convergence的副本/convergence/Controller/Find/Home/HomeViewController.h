6b02eb6faf345f8085499f208932aeff8ea022a7
yang932237153 <932237153@qq.com>
2017-09-22 19:10:20 +0800

    555

---
+++ convergence的副本/convergence/Controller/Find/Home/HomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.h
+//  convergence
+//
+//  Created by admin on 2017/9/5.
+//  Copyright © 2017年 adminadmineducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HomeModel.h"
+@interface HomeViewController : UIViewController
+@property(nonatomic,strong) HomeModel * home;
+
+@end
